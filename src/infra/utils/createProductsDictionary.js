58f8f89522c2d965869a6fd8cda25ec1cd4a096c
Geovane Saraiva da Silva <saraivasilva75@gmail.com>
2023-05-24 15:20:29 +0000

    Produtos alterados

--- src/infra/utils/createProductsDictionary.js
+++ src/infra/utils/createProductsDictionary.js
@@ -5,6 +5,7 @@ const productsJson = require('../../../products.json')
 
 for (const category of productsJson) {
     for (const product of category.products) {
+        console.log(product.description)
         dictionary[product.id] = product.description
         array.push(product.id)
     }
