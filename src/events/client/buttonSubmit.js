b3543df1a2c02a5c26b6ae1d3ec57af4c28811d5
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2023-06-03 12:26:05 +0000

    Agora apenas os tickets do correio elegante terão o o campo Comprador removido

--- src/events/client/buttonSubmit.js
+++ src/events/client/buttonSubmit.js
@@ -25,9 +25,11 @@ module.exports = class extends eventStructure {
                 const shippingChannel = getChannel(channels.shippingChannelId)
 
                 let minimizedTicketEmbed = interaction.message.embeds[0]
-                minimizedTicketEmbed.fields.splice(minimizedTicketEmbed.fields.findIndex(field => field.name == 'Comprador'), 1)
-                minimizedTicketEmbed.fields.splice(minimizedTicketEmbed.fields.findIndex(field => field.name == 'Série do Comprador'), 1)
-
+                if (interaction.message.embeds[0].color === 15895712) {
+                    minimizedTicketEmbed.fields.splice(minimizedTicketEmbed.fields.findIndex(field => field.name == 'Comprador'), 1)
+                    minimizedTicketEmbed.fields.splice(minimizedTicketEmbed.fields.findIndex(field => field.name == 'Série do Comprador'), 1)
+                }
+                
                 shippingChannel.send({ content: '<@&1114199498986618881>', embeds: [minimizedTicketEmbed], components: [Buttons.productSent] })
 
                 try {
