db2fdf44b85efe79bca2bcceaf2781cb0e2c9618
Geovane <saraivasilva75@gmail.com>
2023-06-07 10:41:56 -0300

    Bugifx: tentava modificar string no preço (number)

--- GetTickets.js
+++ GetTickets.js
@@ -2,9 +2,10 @@ const TicketsDB = require('./src/database/TicketsDB')
 require('dotenv').config({ path: './release.env' })
 
 const a = async () => {
+    const date_obj = new Date(Date())
     const tk = new TicketsDB()
     const data = await tk.obterTickets()
     const { writeFileSync } = require('fs')
-    writeFileSync(`${process.cwd()}/json/tickets.json`, JSON.stringify(data, null, 4))
+    writeFileSync(`${process.cwd()}/json/tickets-${date_obj.toISOString()}.json`, JSON.stringify(data, null, 4))
 }
 a()
