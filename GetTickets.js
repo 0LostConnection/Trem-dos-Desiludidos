ab8086e08b84d14f232963ddbfd0ac8668e13cec
Geovane <saraivasilva75@gmail.com>
2023-06-07 10:50:41 -0300

    Exportar função

--- GetTickets.js
+++ GetTickets.js
@@ -1,11 +1,12 @@
 const TicketsDB = require('./src/database/TicketsDB')
 require('dotenv').config({ path: './release.env' })
 
-const a = async () => {
+module.exports = GetTickets = async () => {
     const date_obj = new Date(Date())
     const tk = new TicketsDB()
     const data = await tk.obterTickets()
     const { writeFileSync } = require('fs')
     writeFileSync(`${process.cwd()}/json/tickets-${date_obj.toISOString()}.json`, JSON.stringify(data, null, 4))
 }
-a()
+
+module.exports()
