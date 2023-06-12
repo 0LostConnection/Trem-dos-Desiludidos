f03f3bc770be7dffcb4fb276fb8ef48117ac6c70
Geovane <saraivasilva75@gmail.com>
2023-06-12 17:57:48 -0300

    Função atualizada

--- GetTickets.js
+++ GetTickets.js
@@ -1,10 +1,13 @@
 const TicketsDB = require('./src/database/TicketsDB')
 require('dotenv').config({ path: './release.env' })
 
-const a = async () => {
+module.exports = GetTickets = async () => {
+    const date_obj = new Date(Date())
+    const name = `tickets-${date_obj.toISOString()}.json`
     const tk = new TicketsDB()
     const data = await tk.obterTickets()
     const { writeFileSync } = require('fs')
-    writeFileSync(`${process.cwd()}/json/tickets.json`, JSON.stringify(data, null, 4))
+    writeFileSync(`${process.cwd()}/json/${name}`, JSON.stringify(data, null, 4))
+
+    return name
 }
-a()
